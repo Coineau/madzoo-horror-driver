435d8f28a71a1ecd39062524342500ebb97e891f
sir.mc.ogan@gmail.com <sir.mc.ogan@gmail.com@b07f5670-ba7c-2c0a-7ac8-612ffc54f2f0>
2011-04-12 07:31:36 +0000

    Mise à jour des commentaires de Auto.h et correction de Terrain(->Ca Compile)

--- trunk/src/Auto.h
+++ trunk/src/Auto.h
@@ -12,19 +12,35 @@ typedef struct
 	int nbPlaces;
 } Auto;
 
-
+/** Initialise une structure Auto*/
 void autoInit(Auto *);
+
+/** Les quatres fonctions suivantes déplacent le joueur*/
 void autoGauche(Auto *, const Terrain *);
 void autoDroite(Auto *, const Terrain *);
 void autoHaut(Auto *, const Terrain *);
 void autoBas(Auto *, const Terrain *);
+
+/**Les deux fonctions suivantes récuperent la position du joueur*/
 int autoGetX(const Auto *);
 int autoGetY(const Auto *);
+
+/**Récupère les PV du joueur*/
 int autoGetPdv(const Auto*);
+
+/**Change les PV*/
 void autoSetPdv(Auto*,int);
+
+/** Récupère le nombre de places total*/
 int autoGetNbPlaces(const Auto*);
+
+/** Change le nombre de places total*/
 void autoSetNbPlaces(Auto*,int);
+
+/** Récupère le nombre de places occupées*/
 int autoGetnbSurviDansAuto(const Auto*);
+
+/**Change le nombre de places occupées*/
 void autoSetnbSurviDansAuto(Auto*,int);
 
 
