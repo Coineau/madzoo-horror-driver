9f7b69b2557c334838a3d016a15357a86a9d9f76
dedieu.simon@gmail.com <dedieu.simon@gmail.com@b07f5670-ba7c-2c0a-7ac8-612ffc54f2f0>
2011-05-06 14:10:59 +0000

    ajout de la doc de terrain et get/set pour nbZ

--- trunk/src/Terrain.h
+++ trunk/src/Terrain.h
@@ -3,22 +3,56 @@
 
 typedef struct
 {
-	int dimx;
-	int dimy;
-	char **tab;
-	int nbS;
+        /**Largeur du terrain*/
+        int dimx;        
+        /**Hauteur du terrain*/
+        int dimy;        
+        /**Tableau 2D referant les elements du terrain*/
+        char **tab;        
+        /**Nombre de survivants*/
+        int nbS;        
+        /**Nombre de zombies*/
+        int nbZ;
 } Terrain;
 
+/**Initialise le terrain en important la structure terrain d'un fichier texte*/
 void terInit(Terrain *);
+
+/**Libere le terrain*/
 void terLibere(Terrain *);
+
+/**Verifie que la case n'est pas un mur*/
 int terEstPositionPersoValide(const Terrain *, const int x, const int y);
-const char terGetXY(const Terrain *, const int x, const int y);
-void terSetXY(const Terrain *, const int x, const int y, const char);
+
+/**Verifie que la case est un survivant*/
 int terEstPositionSurvivant(const Terrain*, const int x, const int y);
-const int getDimX(const Terrain *);
-const int getDimY(const Terrain *);
-const int terGetnbS(const Terrain *);
-void terSetnbS(Terrain *, const int);
+
+/**Verifie que la case est un zombie*/
 int terEstPositionZombie(const Terrain*, const int x, const int y);
+
+/**Renvoie le caractere dont les coordonnees sont passees en parametre*/
+const char terGetXY(const Terrain *, const int x, const int y);
+
+/**Modife le caractere dont les coordonnees sont passees en parametre*/
+void terSetXY(const Terrain *, const int x, const int y, const char);
+
+/**Renvoie la largeur du terrain*/
+const int getDimX(const Terrain *);
+
+/**Renvoie la hauteur du terrain*/
+const int getDimY(const Terrain *);
+
+/**Renvoie le nombre total de survivants*/
+const int terGetnbS(const Terrain *);
+
+/**Modife le nombre total de survivants*/
+void terSetnbS(Terrain *, const int);
+
+/**Renvoie le nombre total de zombies*/
+const int terGetnbZ(const Terrain *);
+
+/**Modife le nombre total de zombies*/
+void terSetnbZ(Terrain *, const int);
+
 
 #endif
