cf083c8849810e4e7e2502525be3cffde5379da5
fumeur51 <fumeur51@gmail.com>
2011-05-06 15:38:14 +0000

    modif Zombie en td

--- src/DesZombies.h
+++ src/DesZombies.h
@@ -13,13 +13,30 @@ typedef struct
 	/** le nombre de zombie dans le jeux */
 	int nbz;
 	/** nombre de zombie restant */
-	int zomrest;
+	/*int zomrest;*/
 }DesZombies;
 
-void dZombieInit(DesZombies *);
-void dZombieLibere(DesZombies *);
-void AjouterZombie(DesZombies *, int x , int y, int i);
-void SupprimeZombie(DesZombies *, int i);
-void dZombieDeplacer(DesZombies *, int autoX , int autoY,Terrain *);
+/** initialisation d'un tebleau dinamique et placement des zombies en fonction du terrain */
+void dZombieInit(DesZombies *pdzon,Terrain *pTer);
 
+/**supprime tous les allocation de la structure zombie (fin du jeux , supresion de tous)*/
+void dZombieLibere(DesZombies *pdzon);
+
+/**suprime un zombie dans le tableau dinamique  */
+void SupprimeZombie(DesZombies *pdzon, Zombie* pzon);
+
+/*void dZombieDeplacer(DesZombies *pdzon, int autoX , int autoY,Terrain * pTer);*/
+
+/**Recupere le nombre de zombie*/
+int dzombieGetnbz(const desZombie*);
+
+/**Recupere le nombre de zombie restant*/
+int dzombieGetzomrest(const Zombie*);
+
+
+/**Modifie la valeur du nombre de zombie */
+void dzombieSetnbz(Zombie*,int x);
+
+/**Modifie la valeur du nombre de zombie restant */
+void dzombieSetzomrest(Zombie*,int y);
 #endif
