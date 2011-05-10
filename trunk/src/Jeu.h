72b2d32483f10d233d88b89aa7a11c21f769945d
sir.mc.ogan@gmail.com <sir.mc.ogan@gmail.com@b07f5670-ba7c-2c0a-7ac8-612ffc54f2f0>
2011-05-10 06:21:59 +0000

    Mise à jour des commentaires

--- trunk/src/Jeu.h
+++ trunk/src/Jeu.h
@@ -15,15 +15,21 @@ typedef struct
 	DesZombies dzombies;
 } Jeu;
 
+/** Initialise une structure Jeu*/
 void jeuInit(Jeu *);
+/** Renvoie la structure Terrain contenue dans Jeu*/
 Terrain *jeuGetTerrainPtr(Jeu *);
+/** Renvoie la structure Auto contenue dans Jeu*/
 Auto *jeuGetAutoPtr(Jeu *);
+/** Renvoie la structure Terrain contenue dans Jeu en constante*/
 const Terrain *jeuGetConstTerrainPtr(const Jeu *);
+/** Renvoie la structure Auto contenue dans Jeu en constante*/
 const Auto *jeuGetConstAutoPtr(const Jeu *);
-
+/** Renvoie une lettre en fonction de la touche pressee par l'utilisateur*/
 void jeuActionClavier(Jeu *j, const char);
-
+/** Gère les collisions entre un Survivant et l'Auto*/
 void collisionSurvi(Terrain *pter, DesSurvivants *pdsurvis, Auto *pauto);
+/** Gère les collisions entre un Zombie et l'Auto*/
 void collisionZombie(Terrain *pter, DesZombies *pdzombies, Auto *pauto);
 
 #endif
