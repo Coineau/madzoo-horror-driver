2b8a87bde3759ac1f95b2323286f3fc178e7bf99
sir.mc.ogan <sir.mc.ogan@b07f5670-ba7c-2c0a-7ac8-612ffc54f2f0>
2011-06-03 14:46:49 +0000

    Ajout elements du menu

---
+++ trunk/src/sdlMenu.h
@@ -0,0 +1,25 @@
+#ifndef _SDLMENU_H
+#define _SDLMENU_H
+
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+
+
+typedef struct 
+{
+	Mix_Music *musiquemenu;
+	Mix_Chunk *deplace;
+	SDL_Surface* surface_ecran;
+	SDL_Surface* surface_bg;
+	SDL_Surface* surface_jouer;
+	SDL_Surface* surface_credits;
+        SDL_Surface* surface_quitter;
+} sdlJeu;
+
+void sdlMenuInit(sdlMenu *);
+void sdlMenuAff(sdlMenu *)
+void sdlMenuBoucle(sdlMenu *);
+void sdlMenuLibere(sdlMenu *);
+void sdlMenuDetruit(sdlMenu *);
+
+#endif
