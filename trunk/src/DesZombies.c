d50c8b60da029739c480a8fe213a447829b9ff95
fumeur51@gmail.com <fumeur51@gmail.com@b07f5670-ba7c-2c0a-7ac8-612ffc54f2f0>
2011-06-06 10:34:44 +0000

    rajou instruction dans menuncurs

--- trunk/src/DesZombies.c
+++ trunk/src/DesZombies.c
@@ -33,25 +33,12 @@ void dZombieLibere(DesZombies * pdzon)
 	int i = 0;
 	while(i < pdzon->nbz)
 	{
-		if(pdzon->zombies[i] == NULL );
-		{
 			free(pdzon->zombies[i]);
-			pdzon->zombies[i] = NULL;
-		}
-		i++;
 	}
 	pdzon->nbz=0;
 	free(pdzon);
 }
 
-/*void AjouterZombie(DesZombies * pdzon, terrain *pTer)
-{
-	int  =0;
-	pdzon->zombies[i] = (Zombie*)malloc(sizeof(Zombie));
-	zombieInit(pdzon->zombies[i] , x, y);
-	pdzon->nbz=pdzon->nbz + 1;
-	pdzon->zomrest=pdzon->zomrest + 1;
-}*/
 
 void SupprimeZombie(DesZombies *pdzon ,int autoX ,int autoY , Terrain * pTer)
 {
